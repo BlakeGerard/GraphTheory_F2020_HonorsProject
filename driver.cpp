85c93720c0b819780976147edbaa82ba4f24da45
BlakeGerard <blakegerard@ou.edu>
2020-10-20 19:20:14 -0500

    Examples folder for graph files

--- driver.cpp
+++ driver.cpp
@@ -9,7 +9,8 @@
 
 #include <iostream>
 #include <cstring>
-#include "unistd.h"
+#include <cstdio>
+#include <map>
 #include "Graph.hpp"
 #include "graph_parser.h"
 
@@ -19,24 +20,26 @@ extern FILE *yyin;
 int main(int argc, char **argv) {
 
     FILE *input_graph;
-    input_graph = fopen(argv[2], "r");
+    input_graph = fopen(argv[1], "r");
     if (input_graph) {
         yyin = input_graph;
     } else {
-        std::cout << "Failed to open file" << std::endl;
+        std::cout << "Failed to open file." << std::endl;
         exit(1);
     }
 
-    Graph* g;
+    Graph* g = new Graph();
     yy::graph_parser parser(g);
     int result = parser.parse();
 
-    if (result) {
-        std::cout << "Parsing complete" << std::endl;
+    if (!result) {
+        std::cout << "Parsing complete." << std::endl;
     } else {
-        std::cout << "Parsing failed" << std::endl;
+        std::cout << "Parsing failed." << std::endl;
         exit(2);
     }
+
+    g->print();
     
     return 0;
 };
